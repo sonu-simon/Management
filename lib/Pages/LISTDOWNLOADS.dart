741a09a4d34247c09dc03bc2e1cd6e097d4161bc
mac <mac@macs-MBP.Dlink>
2020-10-09 00:22:15 +0530

    files downloadable

--- lib/Pages/LISTDOWNLOADS.dart
+++ lib/Pages/LISTDOWNLOADS.dart
@@ -1,6 +1,10 @@
+import 'dart:io' as io;
+
 import 'package:cce/models/DOWNLOADABLEFILES.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ListDownloads extends StatefulWidget {
   @override
@@ -11,6 +15,8 @@ List<String> subjects;
 List<DownloadableFile> downloadableFiles;
 
 class _ListDownloadsState extends State<ListDownloads> {
+  io.Directory savedDir;
+
   Future<void> initializeFirestore() async {
     subjects = [];
     downloadableFiles = [];
@@ -38,29 +44,124 @@ class _ListDownloadsState extends State<ListDownloads> {
               moduleName: value['moduleName'],
               fileName: value['fileName'],
               downloadUrl: value['downloadUrl']);
+
           print(downloadableFile.downloadUrl);
+          downloadableFiles.add(downloadableFile);
+          print(downloadableFiles);
+          checkForFiles();
+          setState(() {});
         });
       });
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    initializeFirestore();
+  setWorkingDirectory() async {
+    io.Directory baseDir = await getExternalStorageDirectory();
+    print(baseDir.path);
+    String localPath = baseDir.path + io.Platform.pathSeparator + 'Download';
+    savedDir = io.Directory(localPath);
+    print('savedDir: ${savedDir.path}');
+    bool hasExisted = await savedDir.exists();
+    print(hasExisted);
+    if (!hasExisted) {
+      print('creation //////////////');
+      savedDir.create();
+    }
+    // }
   }
 
   @override
+  void initState() {
+    super.initState();
+    // WidgetsFlutterBinding.ensureInitialized();
+    FlutterDownloader.initialize(
+        debug: true // optional: set false to disable printing logs to console
+        );
+    // initializeFirestore();
+    setWorkingDirectory();
+  }
+
+  Future<bool> checkIfFileExists(String fileName) async {
+    print('${savedDir.path}/$fileName');
+    var returned = await io.File('${savedDir.path}/$fileName').exists();
+    print('returned from file: $returned');
+    if (returned == true) {
+      print('return called');
+      return true;
+    } else
+      return false;
+  }
+
+  checkForFiles() {
+    downloadableFiles.forEach((element) {
+      print('processing: ${element.fileName}');
+      checkIfFileExists(element.fileName).then((value) =>
+          value ? element.isDownloaded = true : element.isDownloaded = false);
+    });
+  }
+
+  Color fileStatus;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: FlatButton(
-        onPressed: initializeFirestore,
-        child: Text(
-          'FIRESTORE',
-          style: TextStyle(fontSize: 30),
+      appBar: AppBar(backgroundColor: Colors.white, elevation: 0.0, actions: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              FlatButton(
+                // color: Colors.amber,
+                onPressed: initializeFirestore,
+                child: Text(
+                  'FILES',
+                  style: TextStyle(fontSize: 30, letterSpacing: 0.5),
+                ),
+              ),
+              FlatButton(
+                // color: Colors.amber,
+                onPressed: initializeFirestore,
+                child: Text(
+                  'FIRESTORE',
+                  style: TextStyle(fontSize: 30, letterSpacing: 0.5),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    ));
+      ]),
+      body: ListView.builder(
+          itemCount: downloadableFiles.length,
+          itemBuilder: (context, index) {
+            print(downloadableFiles[index].isDownloaded);
+
+            return ListTile(
+              tileColor: downloadableFiles[index].isDownloaded
+                  ? Colors.lightGreen[400]
+                  : Colors.orange[300],
+              title: Text(downloadableFiles[index].fileName),
+              subtitle: Text(downloadableFiles[index].moduleName),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                      icon: downloadableFiles[index].isDownloaded
+                          ? Icon(Icons.cloud_download)
+                          : Icon(Icons.cloud_download_outlined),
+                      onPressed: () async {
+                        final taskId = await FlutterDownloader.enqueue(
+                          fileName: downloadableFiles[index].fileName,
+                          url: downloadableFiles[index].downloadUrl,
+                          savedDir: savedDir.path,
+                          showNotification:
+                              true, // show download progress in status bar (for Android)
+                          openFileFromNotification:
+                              true, // click on notification to open downloaded file (for Android)
+                        );
+                      }),
+                ],
+              ),
+            );
+          }),
+    );
   }
 }
--- lib/models/DOWNLOADABLEFILES.dart
+++ lib/models/DOWNLOADABLEFILES.dart
@@ -4,6 +4,7 @@ class DownloadableFile {
   String moduleName;
   String fileName;
   String downloadUrl;
+  bool isDownloaded = false;
 
   DownloadableFile({
     @required this.moduleName,
